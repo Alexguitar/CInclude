5f4a14fcaca131e2b50b982fcb0023556047aacd
Alex <olekgitara@wp.pl>
2015-11-21 16:57:12 +0100

    Bugfix

--- dbg.h
+++ dbg.h
@@ -3,7 +3,7 @@
 
 /* macro definitions */
 #define log_err(M) fprintf(stderr, "[ERROR] %s in %s:%d:\n\t" M "\n", __func__,  __FILE__, __LINE__)
-#define log_warn(M) fprintf(stderr, "[WARN] %s:\n\t" M "\n", __func__,)
+#define log_warn(M) fprintf(stderr, "[WARN] %s:\n\t" M "\n", __func__)
 
 #define errno_print() { fprintf(stderr, "[ERROR] %s in %s:%d errno = %d\n", __func__, __FILE__, __LINE__, errno); perror(NULL); }
 
